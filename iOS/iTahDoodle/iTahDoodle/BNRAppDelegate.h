c95ab448338485f047a98adf4d8778af3165c2fa
Ryan Clark <rclark.sf@gmail.com>
2012-12-11 13:12:21 -0800

    Add iOS folder and iTahDoodle App.

---
+++ iOS/iTahDoodle/iTahDoodle/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  iTahDoodle
+//
+//  Created by Ryan Clark on 12/11/12.
+//  Copyright (c) 2012 Ryan Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/iTahDoodle/iTahDoodle/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iTahDoodle
+//
+//  Created by Ryan Clark on 12/11/12.
+//  Copyright (c) 2012 Ryan Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
