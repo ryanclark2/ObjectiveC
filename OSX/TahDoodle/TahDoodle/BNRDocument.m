65ee1648a16009fbe49339f93c0e470ad2d88e19
Ryan Clark <rclark.sf@gmail.com>
2012-12-13 20:58:20 -0800

    Finish TahDoodle

--- OSX/TahDoodle/TahDoodle/BNRDocument.m
+++ OSX/TahDoodle/TahDoodle/BNRDocument.m
@@ -92,23 +92,47 @@
     return YES;
 }
 
-- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
+- (NSData *)dataOfType:(NSString *)typeName
+                 error:(NSError **)outError
 {
-    // Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-    // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
-    return nil;
+    // This method is called when our document is being saved
+    // We are expected to hand the caller an NSData object wraing our data
+    // so that it can be written to disk
+    
+    // If there's no array, we'll write out an empty array for now
+    
+    if (!todoItems) {
+        todoItems = [NSMutableArray array];
+    }
+    
+    // Pack our todoItems array into an NSData object
+    NSData *data = [NSPropertyListSerialization
+                    dataWithPropertyList:todoItems
+                                  format:NSPropertyListXMLFormat_v1_0
+                                 options:0
+                                   error:outError];
+    
+    // Return our newly-packed NSData object
+    return data;
 }
 
-- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
+- (BOOL)readFromData:(NSData *)data
+              ofType:(NSString *)typeName
+               error:(NSError **)outError
 {
-    // Insert code here to read your document from the given data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning NO.
-    // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead.
-    // If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-    NSException *exception = [NSException exceptionWithName:@"UnimplementedMethod" reason:[NSString stringWithFormat:@"%@ is unimplemented", NSStringFromSelector(_cmd)] userInfo:nil];
-    @throw exception;
-    return YES;
+    // This method is called when a document is being loaded
+    // we are handed an NSData object and expected to pull our data out of it
+    
+    // Extract our todoItems
+    todoItems = [NSPropertyListSerialization
+                        propertyListWithData:data
+                                     options:NSPropertyListMutableContainers
+                                      format:NULL
+                                       error:outError];
+    
+    // return success or failure depending on success of the above call
+    return (todoItems != nil);
+    
 }
 
 @end
