d55477cb403d4d7d509224d7677bf3ca20ceedf0
Ryan Clark <rclark.sf@gmail.com>
2012-12-13 22:31:37 -0800

    Add projects

---
+++ CommandLineTools/AGoodStart/AGoodStart/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Ryan Clark on 12/3/12.
+//  Copyright (c) 2012 Ryan Clark. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    //print the beginning of the novel
+    printf("It was the best of times. \n");
+    printf("It was the worst of times. \n");
+    /*  Is that actually any good?
+        Maybe it needs a rewrite. */
+    
+    return 0;
+}
+
