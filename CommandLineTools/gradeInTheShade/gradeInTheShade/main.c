6f1a32512cfc115b2001f08e961d24b75d78cb72
Ryan Clark <rclark.sf@gmail.com>
2012-12-17 15:32:59 -0800

    refactor C arrays

--- CommandLineTools/gradeInTheShade/gradeInTheShade/main.c
+++ CommandLineTools/gradeInTheShade/gradeInTheShade/main.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h> // malloc(), free()
 
-float averageFloats(float *data, int dataCount)
+float averageFloats(float data[], int dataCount)
 {
     float sum = 0.0;
     for (int i = 0; i < dataCount; i++) {
@@ -21,18 +21,14 @@ float averageFloats(float *data, int dataCount)
 int main(int argc, const char * argv[])
 {
     
-    // Create an array of floats
-    float *gradeBook = malloc(3 * sizeof(float));
-    gradeBook[0] = 60.2;
-    gradeBook[1] = 94.5;
-    gradeBook[2] = 81.1;
+    // Declares the array as part of the frame
+    float gradeBook[] = {60.2, 94.5, 81.1};
     
     // Calculate the average
     float average = averageFloats(gradeBook, 3);
     
-    // Free the array
-    free(gradeBook);
-    gradeBook = NULL;
+    // No need to free the array!
+    // Cleanup happens automatically when the function returns
     
     printf("Average = %.2f\n", average);
 }
