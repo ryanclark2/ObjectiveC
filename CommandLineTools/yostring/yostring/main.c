6d8a245f044c4a9fda82e7d8d94550184088860d
Ryan Clark <rclark.sf@gmail.com>
2012-12-17 15:05:33 -0800

    refactor C Strings

--- CommandLineTools/yostring/yostring/main.c
+++ CommandLineTools/yostring/yostring/main.c
@@ -22,26 +22,17 @@ int main(int argc, const char * argv[])
     // Get a pointer to 5 bytes of memory in the heap
     char *start = malloc(5);
     
-    // Put 'L' in the first byte
-    *start = 'L';
-    
-    // Put 'o' in the second byte
-    *(start + 1) = 'o';
-    
-    // Put 'v' in the third byte
-    *(start + 2) = 'v';
-    
-    // Put 'e' in the fourth byte
-    *(start + 3) = 'e';
-    
-    // Put zero in the fifth byte
-    *(start + 4) = '\0';
+    start[0] = 'L';
+    start[1] = 'o';
+    start[2] = 'v';
+    start[3] = 'e';
+    start[4] = '\0';
     
     // Print out the string and its length
     printf("%s has %zu characters\n", start, strlen(start));
     
     // Print out the third letter
-    printf("The third letter is %c\n", *(start + 2));
+    printf("The third letter is %c\n", start[2]);
     
     // Free the memory so that is can be reused
     free(start);
